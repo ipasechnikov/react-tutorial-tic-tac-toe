58c50cec01079ddc00a41fff0e15853c17966b92
Ilia Pasechnikov <ilia.pasechnikov@gmail.com>
2023-01-30 11:34:52 +0900

    Implement winning logic

--- src/index.js
+++ src/index.js
@@ -21,6 +21,11 @@ class Board extends React.Component {
 
     handleClick(i) {
         const squares = this.state.squares.slice();
+
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+
         squares[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState({
             squares: squares,
@@ -38,8 +43,15 @@ class Board extends React.Component {
     }
 
     render() {
-        const nextPlayer = this.state.xIsNext ? 'X' : 'O';
-        const status = `Next player: ${nextPlayer}`;
+        const winner = calculateWinner(this.state.squares);
+
+        let status;
+        if (winner) {
+            status = `Winner: ${winner}`;
+        } else {
+            const nextPlayer = this.state.xIsNext ? 'X' : 'O';
+            status = `Next player: ${nextPlayer}`;
+        }
 
         return (
             <div>
@@ -78,6 +90,28 @@ class Game extends React.Component {
             </div>
         );
     }
+}
+
+function calculateWinner(squares) {
+    const winnerLines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+
+    for (let i = 0; i < winnerLines.length; i++) {
+        const [a, b, c] = winnerLines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+
+    return null;
 }
 
 // ========================================
